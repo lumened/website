9ad143b1245bd17b73c6dcfa758c68501d239450
Prakash Paudel <total.prakash@gmail.com>
2014-08-08 16:44:36 +0530

    made changes to map and contact form

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
+/*Linking CSS for the google maps iframe*/
 
-google.maps.event.addDomListener(window, 'load', initialize);
+var cssLink = document.createElement("link") 
+    		cssLink.href = "iframe.css"; 
+    		cssLink .rel = "stylesheet"; 
+    		cssLink .type = "text/css"; 
+				
+				var doc=document.getElementById("edit").contentWindow.document 
+
+    		//Then append child
+doc.body.appendChild(cssLink);
