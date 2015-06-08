c2548909b0f0010f3c0bf2ffdbc691adf8aa9afa
Prakash Paudel <total.prakash@gmail.com>
2015-06-08 14:19:59 -0400

    starting new branch

---
+++ new/public/signin.php
@@ -0,0 +1,41 @@
+<?php $page_title = "LumenEd | Sign In";
+$page_description = "Login to see the latest videos and messages from your pen pal";
+?>
+
+<?php include("includes/head.php"); ?>
+	<body class="jobs-page" style="overflow-x:hidden;">
+		<?php include ('includes/nav.php');?>
+
+<!-- 		<div class="container-fluid cover row">
+			<div class="row text-center">
+				<div class="col-sm-10 col-sm-offset-1">
+					<h1>Join the team</h1>
+				</div>
+			</div>
+			<div class="row text-center">
+				<div class="col-xs-12">
+					<a class="btn btn-transparent btn-lg" role="button" href="LumenEdPressKit.zip">Download Press Kit</a>
+				</div>
+			</div>
+		</div> -->
+		
+		<div class="container-fluid orange-panel heading-panel">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<h1 class="heading">Video Pen Pal Program 2015-16</h1>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid white-panel jobs-panel">
+			<div class="row text-center">
+				<div class="col-sm-8 col-sm-offset-2">
+					
+				</div>
+			</div>
+		</div>
+		
+		
+		<?php include ('includes/footer.php');?>
+		<?php include ('includes/scripts.php');?>
+	</body>
+</html>
