401f0fed486e19321bfbb2d66d857d935ff092c2
Prakash Paudel <total.prakash@gmail.com>
2015-06-11 14:17:38 -0400

    Footer fixed

--- new/public/includes/footer.php
+++ new/public/includes/footer.php
@@ -1,3 +1,3 @@
-<footer>
+<footer class="footer">
 	<?php echo "<p class=\"text-left\">&copy; " . date("Y") . " LumenEd, Inc. All Rights Reserved</p>"?>
 </footer>
---
+++ new/public/thankyou.php
@@ -0,0 +1,128 @@
+<?php $page_title = "LumenEd | Thank You";
+$page_description = "Thanks to all of our Kickstarter Backers.";
+?>
+
+<?php include("includes/head.php"); ?>
+	<body class="thankyou-page" style="overflow-x:hidden;">
+		<?php include ('includes/nav.php');?>
+		
+		<div class="container-fluid orange-panel heading-panel">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<h1>A big thank you to the following individuals. Without you our work wouldn't be possible!</h1>
+				</div>
+			</div>
+		</div>
+		
+		<div class="container-fluid orange-panel thankyoulist-panel">
+		<div class="row">
+				<div class="col-xs-12 text-center">
+		<h4>
+					Alex Deeter	|
+			An Evolution At Work	|
+			Ann Deppman	|
+			Anton Elbers	|
+			Aurelie Jacques	|
+			Benjamin Altheimer	|
+			Beth Comstock	|
+			Beth Tallman	|
+			Bill Pohlman	|
+			Bryce Carter	|
+			Catasha Meowow	|
+			Chelsea de Souza	|
+			Claire Halpin	|
+			Danielle Evansic	|
+			David Hellam	|
+			Dean Ginsberg	|
+			Deepa Ranganathan	|
+			Dennis Molesky`	|
+			Dylan Tencic	|
+			Eduardo Rdz	|
+			Elizabeth Corcoran	|
+			Emily Kreek	|
+			Emma Rosen	|
+			Eric	|
+			Eric Estes	|
+			Eric Neuman	|
+			Eric Walters	|
+			Frederik Harboe	|
+			Gabriel Iatarola	|
+			Gene Rostov	|
+			Hannah Belsky	|
+			Harish Mandala	|
+			Hassan Bin Fahim	|
+			Ian Muir	|
+			Izzy	|
+			Jacques Thomas	|
+			James Dellow	|
+			Jane Braziunas	|
+			Jay Rosen	|
+			Jerome Lahey	|
+			Joaquín Ruales	|
+			Karan Kareer	|
+			Kari Iatarola	|
+			Kathy Iatarola	|
+			Katie Chase	|
+			Kevin Smith	|
+			Leena Dahal	|
+			Leslie Grant	|
+			Leslie Kreek	|
+			Lisa Ryno	|
+			Louis Harboe	|
+			Luke Taylor	|
+			Ma'ayan Plaut	|
+			Mark Moritz-Rabson	|
+			Matt Rubinstein	|
+			Max Merrill	|
+			Melissa Wolfish	|
+			Meris L Bergquist	|
+			Michael Claffey	|
+			Michael Jacobs	|
+			Michael Lipman	|
+			Michael Stenovec	|
+			Michelle Mostow	|
+			Mikey Pitts	|
+			Nancy Nguyen	|
+			Nick Petzak	|
+			Nickolas Clarke	|
+			Noahtron Chevan	|
+			Oberlin College LaunchU	|
+			Pablo Horth Rojas	|
+			Patricia West	|
+			Polly Onderko	|
+			Rachelle Martino	|
+			Rand Zalzala	|
+			Robert Cornell	|
+			Robert Palacios	|
+			Robo Durden	|
+			Rommy Lopat	|
+			Ron Cheung	|
+			Sahana Sen	|
+			Scott Vicenzi	|
+			Shiva Mandala	|
+			Smith & Midge Brittingham	|
+			Srijit Ghosh	|
+			Steve and Jody Kelly	|
+			Stevie Kelly	|
+			Susan Sobolewski	|
+			Suzanne Bernstein	|
+			Tahnee Kirk	|
+			Thomas M Harboe	|
+			Thomas W. Bethel	|
+			Tim Simmons	|
+			Timothy Patch	|
+			Tom	|
+			Vicky Bush-Joseph	|
+			Wendy Wee	|
+			Zoë Feder	
+
+		</h4>
+		</div>
+		</div>
+		</div>
+		
+		
+		<?php include ('includes/footer.php');?>
+		<?php include ('includes/scripts.php');?>
+	</body>
+</html>
